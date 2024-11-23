5c5a6f8a709a466fd2bcb51ad16711e09c2493ad
Лихачёва Виктория Михайловна <29436838@s.masi.ru>
2024-11-23 14:20:57 +0300

    add hw

---
+++ seminars/seminar_2/tasks/src/main/java/hw/Program.java
@@ -0,0 +1,26 @@
+package hw;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+public class Program {
+    /*
+    Используя Reflection API, напишите программу,
+    которая выводит на экран все методы класса String.
+     */
+    public static void main(String[] args) {
+        Class<?> stringClass = StringClass.class;
+        Method[] methods = stringClass.getDeclaredMethods();
+        System.out.println("======================Var.1======================");
+        System.out.println("Методы класса StringClass:");
+        for (Method method : methods) {
+            System.out.println(method.getName());
+        }
+
+        System.out.println("======================Var.2======================");
+        System.out.println("Методы класса StringClass:");
+        Arrays.stream(StringClass.class.getMethods())
+                .map(method -> method.getName())
+                .forEach(methodName -> System.out.println(methodName + "\n"));
+    }
+}
