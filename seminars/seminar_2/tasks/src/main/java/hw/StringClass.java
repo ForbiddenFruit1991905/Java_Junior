cccac41cfb4cca2b2fc3525338a6be84ff4ab957
Лихачёва Виктория Михайловна <29436838@s.masi.ru>
2024-11-23 14:20:17 +0300

    some changes

---
+++ seminars/seminar_2/tasks/src/main/java/hw/StringClass.java
@@ -0,0 +1,17 @@
+package hw;
+
+public class StringClass {
+
+    public void method1(String str) {
+        System.out.println("Метод 1: " + str);
+    }
+
+    public void method2(int num) {
+        System.out.println("Метод 2: " + num);
+    }
+
+    public String method3(String input) {
+        return "Метод 3: " + input;
+    }
+    
+}
--- seminars/seminar_2/tasks/src/main/java/task3/Program.java
+++ seminars/seminar_2/tasks/src/main/java/task3/Program.java
@@ -3,9 +3,20 @@ package task3;
 import java.util.UUID;
 
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException {
         Employee user = new Employee("Vika", "email@ya.ru");
         UUID pk = UUID.randomUUID();
         user.setId(pk);
+
+        QueryBuilder queryBuilder = new QueryBuilder();
+        String insertQuery = queryBuilder.buildInsertQuery(user);
+        System.out.printf("Insert Query: %s\n", insertQuery);
+
+        String selectQuery = queryBuilder.buildSelectQuery(Employee.class, pk);
+        System.out.printf("Select Query: %s\n", selectQuery);
+
+        String updateQuery = queryBuilder.buildUpdateQuery(user);
+        System.out.printf("Update Query: %s\n", updateQuery);
+
     }
 }
