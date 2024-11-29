e104a2a2fe9c11c74e9c3e8617821613935d483a
Лихачёва Виктория Михайловна <29436838@s.masi.ru>
2024-11-29 15:15:23 +0300

    some fixes

--- seminars/seminar_3/src/homework/src/main/java/hw/Main.java
+++ seminars/seminar_3/src/homework/src/main/java/hw/Main.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 объектов класса Person. Реализуйте методы для добавления,
 обновления и удаления объектов Person.
  */
+
 public class Main {
     public static void main(String[] args) {
 
